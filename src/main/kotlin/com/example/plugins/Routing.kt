0ec508ea9fdc7b9b85db98a49a225a82b47d939c
artem <artm.ivashkin@gmail.com>
2022-12-01 21:58:47 +0300

    #3 check for correct words

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.core.hasAnagrams
+import com.example.utils.isValidated
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,7 +11,11 @@ fun Application.configureRouting() {
     routing {
         post("/compare") {
             val words = call.parameters[KEY_words] ?: return@post
-            val list = words.split('_')
+            val list = words.split('_').map { it.trim() }
+            if (!list.isValidated()) {
+                call.respondText("Invalid input type")
+                return@post
+            }
             val result = hasAnagrams(list)
             call.respondText("$result")
         }
